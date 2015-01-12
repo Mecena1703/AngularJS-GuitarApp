25c8231f538ab050552db8cb8a1061bd74b43e5e
Mecena1703 <uholaza73@gmail.com>
2015-01-12 21:25:49 +0100

    nobiteti

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,16 @@ var nameSpace = angular.module("GuitarApp", []);
 
 nameSpace.controller("GuitarFunction", ['$scope','$http',function($scope,$http)
 		{    
-			$http.get('http://localhost:8732/service1/guitars/0').success(function(data){
+			$http.get(/*'http://localhost:8732/service1/guitars/0'*/'js/data.json').success(function(data){
+				$scope.guitarVariable = data;
+		});
+
+		}]
+);
+
+nameSpace.controller("GuitarCombo",['$scope','$http',function($scope,$http)
+		{    
+			$http.get(/*'http://localhost:8732/service1/guitars/0'*/'js/data.json').success(function(data){
 				$scope.guitarVariable = data;
 		});
 
