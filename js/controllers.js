1426459e9c00a466aa0877ec7f934847d31bbb45
Mecena1703 <uholaza73@gmail.com>
2015-01-14 12:23:19 +0100

    novosti

--- js/controllers.js
+++ js/controllers.js
@@ -1,19 +1,35 @@
-var nameSpace = angular.module("GuitarApp", []);
+var GuitarControllers = angular.module("GuitarControllers", ['ngAnimate']);
 
-nameSpace.controller("GuitarFunction", ['$scope','$http',function($scope,$http)
+GuitarControllers.controller("ListController", ['$scope','$http',function($scope,$http)
 		{    
-			$http.get(/*'http://localhost:8732/service1/guitars/0'*/'js/data.json').success(function(data){
+			$http.get('http://localhost:8732/service1/guitars/0'/*'js/data.json'*/).success(function(data){
 				$scope.guitarVariable = data;
+				$scope.orderGuitar = 'Name';
 		});
 
 		}]
 );
 
-nameSpace.controller("GuitarCombo",['$scope','$http',function($scope,$http)
-		{    
-			$http.get(/*'http://localhost:8732/service1/guitars/0'*/'js/data.json').success(function(data){
+GuitarControllers.controller("DetailsController", ['$scope','$http','$routeParams',
+	function($scope,$http,$routeParams){
+			$http.get('http://localhost:8732/service1/guitars/0'/*'js/data.json'*/).success(function(data){
 				$scope.guitarVariable = data;
-		});
+				$scope.whichGuitar = $routeParams.guitarID;
 
-		}]
+				/* Guitar details page navigation logic */
+                                /* previous button */
+				if($routeParams.guitarID > 0)
+					$scope.prevGuitar = Number($routeParams.guitarID)-1;
+				else
+					$scope.prevGuitar = $scope.guitarVariable.length-1;
+                                
+                                /* next button */
+				if($routeParams.guitarID < $scope.guitarVariable.length-1)
+					$scope.nextGuitar = Number($routeParams.guitarID)+1;
+				else
+					$scope.nextGuitar = 0;
+ 
+		});
+	}]
+
 );
